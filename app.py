45a8a4d87c6f3da5d567ed9ee745ccf9ab69ac5a
jwillow <jeffcfwon19@hotmail.com>
2020-06-14 00:32:20 -0400

    working streamlit app

--- app.py
+++ app.py
@@ -18,7 +18,12 @@ from utils import *
 
 st.title('Neural TF-Style Transfer')
 
+st.write("This is a small project to showcase my understanding on the topic of Neural Style Transfer (NST). NST builds on the idea that the deep convolution neural network (CNN) learns the 'style' of an image and transfer said style to a content image. Mathematically this is done by minimizing the linear combination of the content and loss functions. In the context of neural style transfer, 'style' is defined as the correlation between activations across channels within the same layer.")
+
+st.write("There are many layers in a deep CNN. To fully capture the 'style' of an image, or the essence of the artist, researchers suggest sampling the outputs of several intermediate network layers. Doing so the style loss function will capture more information in terms of the 'abstractedness' of the style image. And this poses a better minimization problem.")
+
 st.write('Upload a content and style image')
+
 
 upload_content_file = st.file_uploader(
     "Choose a Content Image", type=["png", "jpg", "jpeg"], key='content_file')
@@ -45,3 +50,73 @@ if upload_content_file and upload_style_file is not None:
         caption="Style Image shape: {}".format(content_image.shape),
         use_column_width=True
     )
+
+    st.write("Much of Neural-Style-Transfer builds on the idea that neural network learns the 'style' of the artist and transfer their style to a content image. And minimize content and style loss to the minimum. In the context of neural style transfer, 'style' is defined as the correlation between activations across channels within the same layer.")
+
+    st.write(
+        "'Correlatedness' between channels is done by calculating the Gram matrix")
+
+    content_layers = ['block5_conv2']
+
+    style_layers = ['block1_conv1',
+                    'block2_conv1',
+                    'block3_conv1',
+                    'block4_conv1',
+                    'block5_conv1']
+
+    num_content_layers = len(content_layers)
+    num_style_layers = len(style_layers)
+
+    # Style-component - Create vgg model from selected layers
+    style_representation = vgg_layers(style_layers)
+    style_outputs = style_representation(style_image*255)
+
+    # Create model class instance and use call method
+    extractor = StyleContentModel(style_layers, content_layers)
+    results = extractor(tf.constant(content_image))
+
+    # Set Gram matrices of the StyleImage as target for minimization
+    # (so each gradient descend algorithm can learn the "style")
+    style_target = extractor(style_image)['style']
+    # Set intermediate layer output of a VGG model's with ContentImage input as target
+    # (so GD can generate image with abstracted features from ContentImage)
+    content_target = extractor(content_image)['content']
+
+    # Generate an image to optimize
+    generated_image = tf.Variable(content_image, dtype='float32')
+
+    # Optimizer, style and content weights
+    opt = tf.optimizers.Adam(learning_rate=0.02, beta_1=0.99, epsilon=1e-1)
+    style_weight = 1e-2
+    content_weight = 1e4
+    epochs = 1
+    steps_per_epoch = 100
+    step = 0
+
+    def train_step(image):
+        '''
+        One training step (one iteration of gradient descend)
+        '''
+        with tf.GradientTape() as tape:
+            outputs = extractor(image)
+            loss = style_content_loss(
+                outputs, style_target, content_target, style_weight, content_weight)
+            # calculates gradients based on loss?
+            grad = tape.gradient(loss, image)
+            # apply gradients to optimization algorithm for iamge update
+            opt.apply_gradients([(grad, image)])
+            image.assign(clip_0_1(image))
+
+    for n in range(epochs):
+        for m in range(steps_per_epoch):
+            step += 1
+            train_step(generated_image)
+            print(".", end='')
+        print("Train step: {}".format(step))
+
+    generated_image = tensor_to_image(generated_image)
+
+    st.image(
+        generated_image,
+        caption="Generated image",
+        use_column_width=True)
--- utils.py
+++ utils.py
@@ -14,12 +14,14 @@ def tensor_to_image(tensor):
     '''
     Return a tensor as an image
     '''
+    # Scale pixel values to (0,255) and convert dtype to int
     tensor = tensor*255
     tensor = np.array(tensor, dtype=np.uint8)
+    # Prune extra dimension
     if np.ndim(tensor) > 3:
         assert tensor.shape[0] == 1
         tensor = tensor[0]
-    return PIL.Image.fromarray(tensor)
+    return Image.fromarray(tensor)
 
 
 def load_img(img_byte):
@@ -48,3 +50,136 @@ def load_img(img_byte):
     img = img[tf.newaxis, :]
 
     return img
+
+
+def vgg19():
+    base_model = tf.keras.applications.VGG19(
+        weights='imagenet',
+        include_top=False,
+        #         input_shape=(224, 224, 3),
+        #         input_tensor=tf.keras.layers.Input(shape=(224, 224, 3)),
+    )
+    return base_model
+
+
+def vgg_layers(layer_names):
+    '''
+    INPUT: a list of layer names
+    OUTPUT: a model(functional object: input, output) that returns a list,
+            specifically the output for each layer in layer_names
+    function returns a vgg19 model (functional object) that return output of layer_name - model requires input,
+    '''
+    # grabs the pretrained model, remove classification layer, and freeze the weights
+    vgg = vgg19()
+    vgg.trainable = False
+
+    # store each layer's output of interest to list
+    outputs = [vgg.get_layer(name).output for name in layer_names]
+
+    # model = Model(inputs, outputs)
+    model = tf.keras.Model([vgg.input], outputs)
+    return model
+
+
+def gram_matrix(input_tensor):
+    '''
+    INPUT: input_tensor
+    OUTPUT: the Gram matrix
+    '''
+    # computes the element-wise sum of input tensor
+    result = tf.linalg.einsum('bijc,bijd->bcd', input_tensor, input_tensor)
+    input_shape = tf.shape(input_tensor)
+    num_locations = tf.cast(input_shape[1]*input_shape[2], tf.float32)
+    return result/(num_locations)
+
+
+# define a VGG-model class that handles style and content layers
+class StyleContentModel(tf.keras.models.Model):
+    '''
+    A class VGG Model that handles style and content layers outputs
+    '''
+
+    def __init__(self, style_layers, content_layers):
+        super(StyleContentModel, self).__init__()
+        self.vgg = vgg_layers(style_layers + content_layers)
+        self.style_layers = style_layers
+        self.content_layers = content_layers
+        self.num_style_layers = len(style_layers)
+        self.vgg.trainable = False
+
+    # call Class Method
+    def call(self, inputs):
+        '''
+        INPUT: image 
+        OUTPUT: dictionary of dictionaries with content&style layer:output
+        '''
+        inputs = inputs*255.0
+        preprocessed_input = tf.keras.applications.vgg19.preprocess_input(
+            inputs)
+
+        # Get the layer outputs from vgg model
+        outputs = self.vgg(preprocessed_input)
+
+        # Split style and content outputs by number of layers for each
+        style_outputs, content_outputs = (outputs[:self.num_style_layers],
+                                          outputs[self.num_style_layers:])
+
+        # Extra step for style - calculate gram matrix of each style output layer
+        style_outputs = [gram_matrix(style_output)
+                         for style_output in style_outputs]
+
+        # Store content and style outputs in dictionary
+        content_dict = {content_name: value
+                        for content_name, value
+                        in zip(self.content_layers, content_outputs)}
+
+        style_dict = {style_name: value
+                      for style_name, value
+                      in zip(self.style_layers, style_outputs)}
+
+        return {'content': content_dict, 'style': style_dict}
+
+
+def clip_0_1(image):
+    '''
+    clips image so pixel values remains between (0,1)
+    '''
+    return tf.clip_by_value(image, clip_value_min=0.0, clip_value_max=1.0)
+
+
+# Loss function: linear combination of style and content loss
+def style_content_loss(outputs, style_target, content_target, style_weight=1e-2, content_weight=1e4, num_style_layers=5, num_content_layers=1):
+    '''
+    INPUT: output = model(image)
+    '''
+    style_output = outputs['style']
+    content_output = outputs['content']
+
+    # store style mean-square-error across layers then sum values in list to get
+    # total style loss
+    # tf.add_n() - add all input tensors element-wise
+    # tf.reduce_mean() - 1/m * summation(...)
+    style_loss = tf.add_n([tf.reduce_mean((style_output[name]-style_target[name])**2)
+                           for name in style_output.keys()])
+    style_loss *= style_weight / num_style_layers
+
+    content_loss = tf.add_n([tf.reduce_mean((content_output[name]-content_target[name])**2)
+                             for name in content_output.keys()])
+    content_loss *= content_weight / num_content_layers
+
+    loss = style_loss + content_loss
+    return loss
+
+
+def train_step(image):
+    '''
+    One training step (one iteration of gradient descend)
+    '''
+    with tf.GradientTape() as tape:
+        outputs = extractor(image)
+        loss = style_content_loss(outputs)
+        # calculates gradients based on loss?
+        grad = tape.gradient(loss, image)
+        # apply gradients to optimization algorithm for iamge update
+        opt.apply_gradients([(grad, image)])
+        image.assign(clip_0_1(image))
